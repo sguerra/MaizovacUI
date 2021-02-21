213f4684c62f503ead1684835329b8c0e46fd364
Sergio Guerra <sergioguerra1991@gmail.com>
2021-02-21 02:13:17 -0600

    Added login mock

---
+++ pages/login.vue
@@ -0,0 +1,22 @@
+<template>
+    <vs-row class="login" justify="center" align="center">
+        <vs-col justify="center" w="3">
+            <form class="login__form">
+                <vs-input block label-placeholder="User" class="form-field"/>
+                <vs-input block label-placeholder="Password"  class="form-field"/>
+                <vs-button block>Login</vs-button>
+            </form>
+        </vs-col>
+    </vs-row>
+</template>
+
+<style>
+.login {
+  width: 100vw;
+  height: 100vh;
+}
+
+.form-field {
+    margin-bottom: 1rem;
+}
+</style>
