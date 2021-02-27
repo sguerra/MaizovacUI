631f72dab4f27cc84733333cde157f7aa1c5208e
Sergio Guerra <sergioguerra1991@gmail.com>
2021-02-27 03:51:27 -0600

    Created BaseApi class

---
+++ api/base.ts
@@ -0,0 +1,39 @@
+import axios, { AxiosInstance, AxiosRequestConfig } from "axios"
+
+const axiosInstance = axios.create({baseURL: `${process.env.baseUrl}/v1`})
+
+class BaseApi {
+    private axios: AxiosInstance
+
+    constructor() {
+        this.axios = axiosInstance
+    }
+
+    async _makeRequest(config: AxiosRequestConfig) {
+        try {
+            const response = await this.axios(config)
+
+            return response.data
+        } catch (error) {
+            throw new Error('API error')
+        }
+    }
+
+    async get(url: string) {
+        return this._makeRequest({ method: 'GET', url: url })
+    }
+
+    async post(url: string, data: any) {
+        return this._makeRequest({ method: 'POST', url, data })
+    }
+
+    async patch(url: string, data: any) {
+        return this._makeRequest({ method: 'PATCH', url, data })
+    }
+
+    async delete(url: string) {
+        return this._makeRequest({ method: 'DELETE', url })
+    }
+}
+
+export default BaseApi;
--- nuxt.config.js
+++ nuxt.config.js
@@ -39,4 +39,8 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
+  
+  env: {
+    baseUrl: process.env.BASE_URL || 'http://localhost:3000'
+  }
 }
