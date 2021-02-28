ce30df127c85b1584d8679dee8c6af199d7c47f0
Sergio Guerra <sergioguerra1991@gmail.com>
2021-02-28 05:14:22 -0600

    Integrated binariservice with remote API

--- api/binaryService.ts
+++ api/binaryService.ts
@@ -3,18 +3,9 @@ import BaseApi from './base'
 
 class BinaryService extends BaseApi {
     async execute(operation: BinaryOperation): Promise<number> {
-        switch (operation.type) {
-            case 'addition':
-                return operation.firstOperand + operation.secondOperand
-            case 'substraction':
-                return operation.firstOperand - operation.secondOperand
-            case 'multiplication':
-                return operation.firstOperand * operation.secondOperand
-            case 'division':
-                return operation.firstOperand / operation.secondOperand
-        }
-        // const data = await this.post(operation.type, operation)
-        // return data.result as number
+        const url = `/services/${operation.type}/calculate`
+        const data = await this.post(url, operation)
+        return data.result as number
     }
 }
 
