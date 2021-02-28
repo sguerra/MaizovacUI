5c3a0b6c9b9847c0f33713c12e91590dea6d1d55
Sergio Guerra <sergioguerra1991@gmail.com>
2021-02-28 05:15:56 -0600

    Integrated square roote service with remote API

--- api/squareRootService.ts
+++ api/squareRootService.ts
@@ -2,9 +2,8 @@ import BaseApi from './base'
 
 class SquareRootService extends BaseApi {
     async execute(operand: number): Promise<number> {
-        return Math.sqrt(operand)
-        // const data = await this.post('/services/square_root', operation)
-        // return data.result as number
+        const data = await this.post('/services/square_root/calculate', { operand })
+        return data.result as number
     }
 }
 
